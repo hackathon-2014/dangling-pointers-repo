0a7a5e5bf72010bd021ed8bc617c67f422a11496
hacktavist <hacktavist616@gmail.com>
2014-08-23 16:41:46 -0400

    trying to tweak when accelerometer function is called

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-window.ondevicemotion = function(event){
-   var accelerationY = event.acceleration.y;
-   if(accelerationY > 10){
-     console.log("Moving");
-     hndlr()
-   } else {
-     console.log("Still");
-   }
+window.ondevicemotion = function(event) {
+
+  var accelerationY = event.acceleration.y,
+    seconds = secondsPassed();
+  if (seconds < 5) {
+    if (accelerationY > 10) {
+      console.log("Moving");
+      hndlr()
+    } else {
+      console.log("Still");
+    }
+  }
 }
 
+
+function secondsPassed() {
+  var date = new Date(),
+    s = date.getSeconds();
+  return s;
+}
 
 function hndlr(response) {
   for (var i = 0; i < response.items.length; i++) {
